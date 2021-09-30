cf629c578ac2cd6a7f3b6409b460ba78422a16e7
Nicolas.p <nicolas014.p@gmail.com>
2021-09-30 12:46:24 -0300

    Se centra la imagen con el personaje usando CSS

--- docs/script.js
+++ docs/script.js
@@ -2,33 +2,35 @@ let elemento = document.getElementById("elemento")
 let cantidadY = 0
 let cantidadX = 0
 let imagen = document.getElementById("imagen_movimiento")
+
+// Funcion para mover el personaje
 window.addEventListener("keydown", (e)=>{
-    let movimiento = 50;
+    let movimiento = 3.125;
     switch (e.key) {
         case "ArrowUp":
             console.log("arriba")
             cantidadY-=movimiento
-            elemento.style.transform=`translate(${cantidadX}px, ${cantidadY}px)`
+            elemento.style.transform=`translate(${cantidadX}em, ${cantidadY}em)`
             break;
         case "ArrowDown":
             console.log("abajo")
             cantidadY+=movimiento
-            elemento.style.transform=`translate(${cantidadX}px, ${cantidadY}px)`
+            elemento.style.transform=`translate(${cantidadX}em, ${cantidadY}em)`
             break;
         case "ArrowLeft":
             console.log("izquierda")
             cantidadX-=movimiento
             imagen.src = "cerdo_LEFT.png"
-            elemento.style.transform=`translate(${cantidadX}px, ${cantidadY}px)`
+            elemento.style.transform=`translate(${cantidadX}em, ${cantidadY}em)`
             break;
         case "ArrowRight":
             console.log("derecha")
             cantidadX+=movimiento
             imagen.src = "cerdo_RIGHT.png"
-            elemento.style.transform=`translate(${cantidadX}px, ${cantidadY}px)`
+            elemento.style.transform=`translate(${cantidadX}em, ${cantidadY}em)`
             break;
         default:
             break;
-    }console.log(cantidad);
+    }console.log(cantidadY, cantidadX);
 })
 
